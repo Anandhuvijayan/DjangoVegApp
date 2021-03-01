81b20803cded38affc335fbb715dda9f62f8f782
Anandhu Vijayan <38665597+Anandhuvijayan@users.noreply.github.com>
2021-03-01 10:10:02 +0100

    Update urls.py

--- vegproject/vegapp/urls.py
+++ vegproject/vegapp/urls.py
@@ -5,12 +5,12 @@ from . import views
 
 
 urlpatterns = [
-    # path('', views.index, name='index'),
+    
     path('', views.IndexView.as_view(), name='index'),
     path('about/', views.AboutView.as_view(), name='about'),
     path('blog/', views.BlogView.as_view(), name='blog'),
     path('contact/', views.ContactView.as_view(), name='contact'),
-    # path('contactform/', views.contactform, name='contactform'),
+    
 
     path('response/', views.responseform, name='responseform' ),
     path('thankyou/', views.responseform, name='responseform'),   
